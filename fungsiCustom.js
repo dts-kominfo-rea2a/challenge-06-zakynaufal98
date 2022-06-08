ffb43a2f7a974ff1d6a2ec6b12efffb3d60cdcb8
zakynaufal98 <zakynaufal1998@gmail.com>
2022-06-08 13:02:33 +0700

    feat:Update Challange 06

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let hasilData = [];
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    } else {
+      let parseResult = JSON.parse(data);
+
+      let resultSplit = parseResult.message.split(" ");
+      hasilData.push(resultSplit[1]);
+    }
+  });
+  fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    } else {
+      let parseResult2 = JSON.parse(data);
+
+      let resultSplit2 = parseResult2[0].message.split("halo ").join("");
+      hasilData.push(resultSplit2);
+    }
+  });
+  fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    } else {
+      let parseResult3 = JSON.parse(data);
+      let resultSplit3 = parseResult3[0]["data"].message
+        .split("halo ")
+        .join("");
+      hasilData.push(resultSplit3);
+      fnCallBack(null, hasilData);
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
